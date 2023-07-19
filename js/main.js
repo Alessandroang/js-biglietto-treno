8419092320b8b077d305af696c6f85d7cbf1ccbe
Alessandroang <alessandroang94@gmail.com>
2023-07-19 18:33:33 +0200

    Add main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-const etadelpasseggero = prompt("Quanti anni hai?");
+const etaDelPasseggero = prompt("Quanti anni hai?");
+const eta = parseInt(etaDelPasseggero);
 
-const kmdapercorrere = prompt("Quanti km devi percorrere?");
+const kmDaPercorrere = prompt("Quanti km devi percorrere?");
+const km = parseInt(kmDaPercorrere);
+
+let prezzoKm = km * 0.21;
+
+if (eta < 18) {
+  let scontoVenti = (prezzoKm / 100) * 20;
+  prezzoKm = prezzoKm - scontoVenti;
+} else if (eta > 65) {
+  let scontoQuaranta = (prezzoKm / 100) * 40;
+  prezzoKm = prezzoKm - scontoQuaranta;
+}
+
+document.getElementById("pluto").innerHTML = "prezzo finale: " + prezzoKm;
